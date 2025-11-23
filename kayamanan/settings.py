4a25eba30837b6d383062f96c67b539b882d725c
PsychoQuake7 <itscalaisha@gmail.com>
2025-11-24 05:08:39 +0800

    mysql database

--- kayamanan/settings.py
+++ kayamanan/settings.py
@@ -82,13 +82,27 @@ WSGI_APPLICATION = 'kayamanan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',  
+        'NAME': 'kayamanan',        
+        'USER': 'root',             
+        'PASSWORD': 'calaisha',         
+        'HOST': '127.0.0.1',                 
+        'PORT': '3306',                      
+        'OPTIONS': {
+            'autocommit': True,
+            'use_pure': True,               
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
