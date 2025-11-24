ce20979024c907a914f5891992a214f153a94bd0
JCREMANDABAN <202380127@psu.palawan.edu.ph>
2025-11-24 23:28:49 +0800

    Update allowed host settings.py

--- kayamanan/settings.py
+++ kayamanan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8bqdq%*g40dp=5e)qpam)eoyn5k38wqg$oqu2+e$cvgu4@z%-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jcpogi.pythonanywhere.com']
 
 
 # Application definition
